decb28fd2ab49c1ffb1678c2b0f29dd908429fa4
minons1 <salimbinusman1@gmail.com>
2024-01-21 23:37:44 +0700

    feat: refactor participant_name_arrays approach

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { Button, Checkbox, Container, Flex, Grid, PasswordInput, Table, Text, TextInput, Title } from '@mantine/core'
 import { jadwal, participant, slot } from '@prisma/client'
-import { useEffect, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import { getDateInterval, getTimeInterval } from '../../util/time'
 import { useForm } from '@mantine/form'
 import { notifications } from '@mantine/notifications'
@@ -13,8 +13,10 @@ type JadwalType = Omit<jadwal, 'start_date' | 'end_date' | 'start_time' | 'end_t
   start_time: string
   end_time: string
   slots: (Omit<slot, 'epoch'> & {
-    epoch: string
-    participants_name_array: string[]
+    epoch: string,
+    participants: {
+      participant_id: string
+    }[]
   })[]
 }
 
@@ -27,7 +29,7 @@ type ParticipantType = Pick<participant, 'id' | 'name'>
 
 export default function Jadwal({ params }: { params: { id: string } }) {
   const [jadwal, setJadwal] = useState<JadwalType | null>(null)
-  const [slotMap] = useState<Map<string, { id: string, participants_name_array: string[] }>>(new Map())
+  const [slotMap] = useState<Map<string, { id: string, participants: string[] }>>(new Map())
   const [isLoading, setLoading] = useState(false)
   const [participant, setParticipant] = useState<ParticipantType | null>(null)
 
@@ -48,8 +50,7 @@ export default function Jadwal({ params }: { params: { id: string } }) {
             name: values.name.toLowerCase(),
             password: values.password
           }
-        }),
-
+        })
       })
 
       const resBody = await res.json()
@@ -68,20 +69,51 @@ export default function Jadwal({ params }: { params: { id: string } }) {
     }
   }
 
+  const handleCheckboxChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    if (!participant) return
+
+    const epoch = e.target.value
+    const slot = slotMap.get(epoch)
+
+    if (!slot) return
+
+    if (e.target.checked) {
+    } else {
+    }
+    slotMap.set(epoch, slot)
+  }
+
   useEffect(() => {
     if (!params.id) return
-    fetch(`/api/jadwal/${params.id}`)
-      .then((res) => res.json())
-      .then((res: { jadwal: JadwalType }) => {
-        setJadwal(res.jadwal)
 
-        for (const slot of res.jadwal.slots) {
-          slotMap.set(slot.epoch, {
-            id: slot.id,
-            participants_name_array: slot.participants_name_array,
-          })
-        }
-      })
+    const fetchJadwal = async () => {
+      const res = await fetch(`/api/jadwal/${params.id}`)
+      if (res.status >= 400) {
+        notifications.show({
+          title: 'Error',
+          message: 'Jadwal not found',
+          color: 'red'
+        })
+        return
+      }
+
+      const resBody = await res.json() as { jadwal: JadwalType}
+      setJadwal(resBody.jadwal)
+
+      const participantRes = await fetch(`/api/jadwal/${params.id}/participants`)
+      if (participantRes.status >= 400) return
+
+      const participantResBody = await participantRes.json() as { participants: ParticipantType[] }
+
+      for (const slot of resBody.jadwal.slots) {
+        slotMap.set(slot.epoch, {
+          id: slot.id,
+          participants: slot.participants.map(par => participantResBody.participants.find(p => p.id === par.participant_id)?.name || '')
+        })
+      }
+    }
+
+    fetchJadwal()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
@@ -94,68 +126,60 @@ export default function Jadwal({ params }: { params: { id: string } }) {
           </Flex>
           <Grid mt='md' align='center'>
             <Grid.Col span={{ base: 12, md: 6 }}>
-            {participant
-              ? <>
-                <Table striped>
-                  <Table.Thead>
-                    <Table.Tr>
-                      <Table.Th>Time / Date</Table.Th>
-                      {getDateInterval(jadwal.start_date, jadwal.end_date, jadwal.timezone).map((date) => {
-                        return date && <Table.Th key={date.toISO()}>
-                            {date.toFormat('ccc d MMM ')}
-                          </Table.Th>
-                      })}
-                    </Table.Tr>
-                  </Table.Thead>
-                  <Table.Tbody>
-                    {getTimeInterval(jadwal.start_time, jadwal.end_time).map((time) => {
-                        return time && <Table.Tr key={time.toISO()}>
-                          <Table.Td>{time.toString().slice(-13, -8)}</Table.Td>
-                          {getDateInterval(jadwal.start_date, jadwal.end_date, jadwal.timezone).map((date) => {
-                            return date && <Table.Td key={`${date.toISO()}${time.toISO()}`}>
-                                <Checkbox
-                                  value={slotMap.get(date.set({ hour: time.hour, minute: time.minute, second: time.second })
-                                        .toUTC()
-                                        .toISO() as string)?.id}
-                                  onChange={(e) => {
-                                    if (e.currentTarget.checked) {
-                                    }
-                                  }}
-                                />
-                              </Table.Td>
-                          })}
-                        </Table.Tr>
-                      }
-                    )}
-                  </Table.Tbody>
-                </Table>
-              </>
-              : <>
-                <Text fw={700}>Participate</Text>
-                <form onSubmit={form.onSubmit(handleSubmitParticipant)}>
-                  <TextInput
-                    size='sm'
-                    mt='md'
-                    placeholder='name'
-                    w='50%'
-                    {...form.getInputProps('name')}
-                  />
-                  <PasswordInput
-                    size='sm'
-                    mt='md'
-                    placeholder='password (optional)'
-                    w='50%'
-                    {...form.getInputProps('password')}
-                  />
-                  <Button
-                    loading={isLoading}
-                    size='sm'
-                    mt='md'
-                    type='submit'
-                  >Sign In</Button>
-                </form>
-              </>
-              }
+            {participant ? <>
+              <Table striped>
+                <Table.Thead>
+                  <Table.Tr>
+                    <Table.Th>↓ Time / Date →</Table.Th>
+                    {getDateInterval(jadwal.start_date, jadwal.end_date, jadwal.timezone).map((date) => {
+                      return date && <Table.Th key={date.toISO()}>
+                          {date.toFormat('ccc d MMM')}
+                        </Table.Th>
+                    })}
+                  </Table.Tr>
+                </Table.Thead>
+                <Table.Tbody>
+                  {getTimeInterval(jadwal.start_time, jadwal.end_time).map((time) => {
+                      return time && <Table.Tr key={time.toISO()}>
+                        <Table.Td>{time.toString().slice(-13, -8)}</Table.Td>
+                        {getDateInterval(jadwal.start_date, jadwal.end_date, jadwal.timezone).map((date) => {
+                          return date && <Table.Td key={date.set({ hour: time.hour, minute: time.minute, second: time.second }).toUTC().toISO()}>
+                              <Checkbox
+                                value={date.set({ hour: time.hour, minute: time.minute, second: time.second }).toUTC().toISO() as string}
+                                onChange={handleCheckboxChange}
+                              />
+                            </Table.Td>
+                        })}
+                      </Table.Tr>
+                    }
+                  )}
+                </Table.Tbody>
+              </Table>
+            </> : <>
+              <Text fw={700}>Participate</Text>
+              <form onSubmit={form.onSubmit(handleSubmitParticipant)}>
+                <TextInput
+                  size='sm'
+                  mt='md'
+                  placeholder='name'
+                  w='50%'
+                  {...form.getInputProps('name')}
+                />
+                <PasswordInput
+                  size='sm'
+                  mt='md'
+                  placeholder='password (optional)'
+                  w='50%'
+                  {...form.getInputProps('password')}
+                />
+                <Button
+                  loading={isLoading}
+                  size='sm'
+                  mt='md'
+                  type='submit'
+                >Sign In</Button>
+              </form>
+            </>}
             </Grid.Col>
             <Grid.Col span={{ base: 12, md: 6 }}>
               This will show current participant
---
+++ src/app/api/jadwal/[id]/participants/route.ts
@@ -0,0 +1,27 @@
+import { Prisma } from '../../../../../lib/prisma'
+
+export async function GET(req: Request, { params }: { params: { id: string } }) {
+  const { id } = params
+
+  const jadwal = await Prisma.jadwal.findUnique({
+    where: {
+      id
+    }
+  })
+
+  if (!jadwal) {
+    return Response.json({ message: 'Jadwal not found' }, { status: 404 })
+  }
+
+  const participants = await Prisma.participant.findMany({
+    where: {
+      jadwal_id: id
+    },
+    select: {
+      id: true,
+      name: true
+    }
+  })
+
+  return Response.json({ participants })
+}
